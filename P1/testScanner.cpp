7b1c8ec2081b28c0472dc02fe10855e72d28a0df
tmkbf9 <48497061+tmkbf9@users.noreply.github.com>
2020-05-28 21:11:26 -0500

    Fixed testScanner

--- P1/testScanner.cpp
+++ P1/testScanner.cpp
@@ -8,6 +8,9 @@ using namespace std;
 
 void TestScanner::testScan(istream & inputSource, ostream & errorStream) {
   token token;
+  istringstream is;
+  ostringstream os;
+  Scanner scanner(is, os);
   do {
     token = scanner.scanner();
     cout << token << endl;
