eff4c9540ef6bdfd95f5b27a4989460ece051ab8
tmkbf9 <48497061+tmkbf9@users.noreply.github.com>
2020-05-28 21:09:00 -0500

    Added new token types

--- P1/token.h
+++ P1/token.h
@@ -7,6 +7,7 @@ struct token {
   static token EOF_Token(int linenumber) { return token("EOFTK", "", linenumber); }
   static token NUM_Token(std::string digits, int linenumber) { return token("NUMTK", digits, linenumber); }
   static token ID_Token(std::string identifier, int linenumber) { return token("IDTK", identifier, linenumber); }
+  static token SYM_Token(std::string symbol, int linenumber) { return token("SYMTK", symbol, linenumber); }
   static token ERR_Token(std::string errorString, int linenumber) { return token("ERRTK", errorString, linenumber); }
 
   token() {}
@@ -28,3 +29,4 @@ private:
 std::ostream & operator<<(std::ostream & os, const token & t);
 
 #endif //P1_TOKEN_H
+
