8dcabffe90656181e8a1a03008411d8505a57525
tmkbf9 <48497061+tmkbf9@users.noreply.github.com>
2020-05-28 21:09:52 -0500

    Added more tests

--- P1/scanner_test.cpp
+++ P1/scanner_test.cpp
@@ -27,7 +27,6 @@ void test_only_one_space_produces_eof_token() {
   Scanner scanner(is, os);
 
   token token = scanner.scanner();
-  
   assert(token.tokenID == "EOFTK");
   assert(token.linenumber == 0);
 }
@@ -52,6 +51,17 @@ void test_only_newlines_produces_eof_token_with_correct_linenumber() {
   
   assert(token.tokenID == "EOFTK");
   assert(token.linenumber == 3);
+}
+
+void test_only_comment_produces_eof_token() {
+    istringstream is("%aaa%");
+    ostringstream os;
+    Scanner scanner(is, os);
+
+    token token = scanner.scanner();
+
+    assert(token.tokenID == "EOFTK");
+    assert(token.linenumber == 0);
 }
 
 // single character tests
@@ -80,12 +90,28 @@ void test_EOF_token_seen_after_last_token_read() {
   assert(token.linenumber == 0);
 }
 
-void test_single_operator_produces_op_token() {
-  
+void test_single_symbol_produces_symbol_token() {
+    istringstream is("[");
+    ostringstream os;
+    Scanner scanner(is, os);
+
+    token token = scanner.scanner();
+
+    assert(token.tokenID == "SYMTK");
+    assert(token.tokenLiteral == "[");
+    assert(token.linenumber == 0);
 }
 
-void test_single_equals_produces_op_token() {
-  
+void test_single_equals_produces_eq_token() {
+    istringstream is("=");
+    ostringstream os;
+    Scanner scanner(is, os);
+
+    token token = scanner.scanner();
+
+    assert(token.tokenID == "SYMTK");
+    assert(token.tokenLiteral == "=");
+    assert(token.linenumber == 0);
 }
 
 // multi-character tests
@@ -119,11 +145,22 @@ void test_two_valid_characters_create_id_token() {
   Scanner scanner(is, os);
 
   token token = scanner.scanner();
-  cout << token << endl;
   
   assert(token.tokenID == "IDTK");
   assert(token.tokenLiteral == "aa");
   assert(token.linenumber == 0);
+}
+
+void test_double_equals_produces_ass_token() {
+    istringstream is("==");
+    ostringstream os;
+    Scanner scanner(is, os);
+
+    token token = scanner.scanner();
+
+    assert(token.tokenID == "SYMTK");
+    assert(token.tokenLiteral == "==");
+    assert(token.linenumber == 0);
 }
 
 // error tests
@@ -142,6 +179,30 @@ void test_single_lcase_char_produces_error() {
 }
 
 void test_single_ucase_char_produces_error() {
+    istringstream is("A");
+    ostringstream os;
+    Scanner scanner(is, os);
+
+    token token = scanner.scanner();
+
+    ostringstream errorString;
+
+    assert(token.tokenID == "ERRTK");
+    assert(token.tokenLiteral == "Error at line 0: Invalid token of starting with capital letter <A>");
+    assert(token.linenumber == 0);
+}
+
+void test_invalid_char_produces_error() {
+    istringstream is("(");
+    ostringstream os;
+    Scanner scanner(is, os);
+
+    token token = scanner.scanner();
+    ostringstream errorString;
+
+    assert(token.tokenID == "ERRTK");
+    assert(token.tokenLiteral == "Error at line 0: Invalid character <(>");
+    assert(token.linenumber == 0);
 }
 
 int main(int argc, char ** argv) {
@@ -155,6 +216,12 @@ int main(int argc, char ** argv) {
   test_two_digits_followed_by_symbol_create_digit_token();
   test_single_lcase_char_produces_error();
   test_two_valid_characters_create_id_token();
+  test_single_ucase_char_produces_error();
+  test_single_symbol_produces_symbol_token();
+  test_single_equals_produces_eq_token();
+  test_double_equals_produces_ass_token();
+  test_only_comment_produces_eof_token();
+  test_invalid_char_produces_error();
 
   return 0;
 }
